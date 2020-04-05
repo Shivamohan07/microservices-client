f7993ca6c2d4d5fc5de09dc2962ca200e99eebbc
shivmoha <shivamohan07@gmail.com>
2020-04-05 19:58:21 +0530

    refresh

--- src/main/java/com/impromptu/microservicesclient/ModuleController.java
+++ src/main/java/com/impromptu/microservicesclient/ModuleController.java
@@ -2,11 +2,13 @@ package com.impromptu.microservicesclient;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RefreshScope
 public class ModuleController {
 
     @Value("${prop1}")
@@ -18,11 +20,16 @@ public class ModuleController {
     @Value("${prop3}")
     String prop3;
 
+    @Value("${constring}")
+    String constring;
+
+
     @RequestMapping("/component")
     public String getProp1(Model m){
         m.addAttribute("property1",prop1);
         m.addAttribute("property2",prop2);
         m.addAttribute("property3",prop3);
+        m.addAttribute("constring",constring);
         return "componentview";
     }
 }
