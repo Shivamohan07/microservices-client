df1dd7e0eab157e1ab16c5fc119a6e3ffc9638a3
shivmoha <shivamohan07@gmail.com>
2020-04-06 08:58:24 +0530

    secure ui

--- src/main/java/com/impromptu/microservicesclient/ModuleController.java
+++ src/main/java/com/impromptu/microservicesclient/ModuleController.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RefreshScope
+//@Controller
+//@RefreshScope
 public class ModuleController {
 
     @Value("${prop1}")
